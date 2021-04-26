43ee2930e7db514502afbd5d4b7b747696dd0e86
JehPark <qkrwpgjs1230@naver.com>
2021-04-26 20:24:42 +0900

    ft_strnequ.c

--- libft/srcs/ft_strnequ.c
+++ libft/srcs/ft_strnequ.c
@@ -6,10 +6,29 @@
 /*   By: jehpark <jehpark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/26 19:58:24 by jehpark           #+#    #+#             */
-/*   Updated: 2021/04/26 20:08:55 by jehpark          ###   ########.fr       */
+/*   Updated: 2021/04/26 20:22:46 by jehpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strnequ(char const )
+int		ft_strnequ(char const *s1, char const *s2, size_t n)
+{
+	int i;
+	int	ans;
+
+	i = 0;
+	while (*s1 && *s1 == *s2 && i < n)
+	{
+		s1++;
+		s2++;
+		i++;
+	}
+	if (i == n)
+	{
+		s1--;
+		s2--;
+	}
+	ans (*s1 - *s2) == 0 ? 1 : 0;
+	return (ans);
+}
