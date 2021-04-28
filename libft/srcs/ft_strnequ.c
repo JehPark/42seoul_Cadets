b95a64bed06c1b9905fea8e63758c16074fe7208
JehPark <qkrwpgjs1230@naver.com>
2021-04-28 14:14:25 +0900

    #3 Issue Check

--- libft/srcs/ft_strnequ.c
+++ libft/srcs/ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: jehpark <jehpark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/26 19:58:24 by jehpark           #+#    #+#             */
-/*   Updated: 2021/04/27 10:39:45 by jehpark          ###   ########.fr       */
+/*   Updated: 2021/04/28 14:14:02 by jehpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 	int i;
 	int	ans;
 
+	if (n == 0)
+		return (1);
 	i = 0;
 	while (*s1 && *s1 == *s2 && (size_t)i < n)
 	{
