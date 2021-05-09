be507088bb271f8d403067337edc5b0fb974a8dc
JehPark <qkrwpgjs1230@naver.com>
2021-05-09 20:22:48 +0900

    gnl started

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jehpark <jehpark@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/09 20:14:18 by jehpark           #+#    #+#             */
+/*   Updated: 2021/05/09 20:16:40 by jehpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+
+#endif
