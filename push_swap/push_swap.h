8f8285bc22c23bef7edfa9778f8c163be1998cca
JehPark <qkrwpgjs1230@naver.com>
2021-05-16 20:25:46 +0900

    push_swap started

---
+++ push_swap/push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jehpark <jehpark@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/16 20:08:31 by jehpark           #+#    #+#             */
+/*   Updated: 2021/05/16 20:10:20 by jehpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct 		s_node
+{
+	int		data;
+	t_node	next;
+}					t_node;
+
+#include <unistd.h>
+#include <stdlib.h>
+
+#endif
---
+++ push_swap/stack.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jehpark <jehpark@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/16 20:06:35 by jehpark           #+#    #+#             */
+/*   Updated: 2021/05/16 20:25:31 by jehpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_node		newNode(int data)
+{
+	t_node	*new_node;
+
+	new_node = (t_node *)malloc(sizeof(struct t_node));
+	new_node->data = data;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+int			isEmpty(t_node *root)
+{
+	return (!root);
+}
+
+void		push(t_node **root, int data)
+{
+	t_node	node;
+
+	node = newNode(data);
+	node->next = *root;
+	*root = stackNode;
+}
+
+int			pop(t_node **root)
+{
+	t_node 	*temp;
+	int		ret;
+
+	if (isEmpty(*root))
+		return (-2147483648);
+	temp = *root;
+	*root = (*root)->next;
+	ret = temp->data;
+	free(temp);
+	return (ret);
+}
+
+int			peek(t_node *root)
+{
+	if (isEmpty(root))
+		return (-2147483648);
+	return (root->data);
+}
